f3689cf0e608c41252c15aa06cff878e5825dbb2
schuemie <schuemie@ohdsi.org>
2015-06-04 15:24:23 +0800

    Initial upload

---
+++ R/CodeChecking.R
@@ -0,0 +1,146 @@
+# @file OhdsiRTools.R
+#
+# Copyright 2015 Observational Health Data Sciences and Informatics
+#
+# This file is part of OhdsiRTools
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' @export
+ohdsiLintrFile <- function(file){
+  lintr::lint(file, linters = list(assignment_linter = assignment_linter,
+                                                       object_snake_case_linter = object_snake_case_linter,
+                                                       commas_linter = commas_linter,
+                                                       infix_spaces_linter = infix_spaces_linter,
+                                                       no_tab_linter = no_tab_linter,
+                                                       object_usage_linter = object_usage_linter,
+                                                       object_multiple_dots_linter = object_multiple_dots_linter,
+                                                       object_length_linter = object_length_linter,
+                                                       open_curly_linter = open_curly_linter,
+                                                       single_quotes_linter = single_quotes_linter,
+                                                       spaces_inside_linter = spaces_inside_linter,
+                                                       trailing_blank_lines_linter = trailing_blank_lines_linter,
+                                                       trailing_whitespace_linter = trailing_whitespace_linter))
+}
+
+#' @export
+ohdsiLintrFolder <- function(path = ".", recursive = TRUE){
+flist <- list.files(path, pattern = "\\.[Rr]$", full.names = TRUE, recursive = recursive)
+  for (f in flist) {
+    message("Auto code formatting ", f)
+    .formatRFile(f)
+  }
+}
+
+.getFunctionDefinitionFromMem <- function(note){
+  funcPos <- regexpr("^.*: ",note)
+  func <- substr(note, funcPos, funcPos + attr(funcPos, "match.length") - 3)
+  func <- gsub("\\s","", func)
+  funcDef <- capture.output(getFunction(func,mustFind=FALSE))
+  if (funcDef[1] == "NULL")
+    return(NULL)
+  else
+    return (funcDef)
+}
+
+.getVariableName <- function(note) {
+  varPos <- regexpr("(variable|parameter) .[a-zA-Z0-9_.-]*.", note)
+  var <- substr(note, varPos + 10, varPos + attr(varPos, "match.length") - 2)
+  return(var)
+}
+
+#' @export
+checkUsagePackage <- function(package,
+                                 ignoreHiddenFunctions = TRUE,
+                                 suppressBindingKeywords = c("ggplot2", "ffwhich", "subset.ffdf", "glm")) {
+  require(package, character.only = TRUE)
+  notes <- capture.output(codetools::checkUsagePackage(package,
+                                                       suppressLocal = FALSE,
+                                                       suppressParamAssigns = TRUE,
+                                                       suppressParamUnused = FALSE,
+                                                       suppressFundefMismatch = FALSE,
+                                                       suppressLocalUnused = FALSE,
+                                                       suppressNoLocalFun = FALSE,
+                                                       skipWith = TRUE,
+                                                       suppressUndefined = FALSE,
+                                                       suppressPartialMatchArgs = FALSE))
+  if (length(notes) == 0) {
+    writeLines("No problems found")
+    return()
+  }
+  newNotes <- c()
+  for (i in 1:length(notes)) {
+    if (regexpr("no visible binding for global variable", notes[i]) != -1) {
+      filePos <- regexpr(" \\(.*\\.R:", notes[i])
+      if (filePos != -1) {
+        # Option 1: use file name and line number to get offending text:
+        file <- substr(notes[i],filePos + 2, filePos + attr(filePos, "match.length") - 2)
+        linePos <- regexpr("\\.R:.*\\)", notes[i])
+        line <- substr(notes[i], linePos + 3, linePos + attr(linePos, "match.length") - 2)
+        line <- strsplit(line, "-")[[1]]
+        if (length(line) == 1) {
+          line <- as.integer(line)
+        } else {
+          line <- as.integer(line[1]):as.integer(line[2])
+        }
+        text <- readLines(file)[line]
+      } else {
+        # Option 2: Get function definition from memory, and select lines with variable name
+        funcDef <- .getFunctionDefinitionFromMem(notes[i])
+        variableName <- .getVariableName(notes[i])
+        text <- funcDef[grep(paste("(^|[^$])", variableName, sep = ""), funcDef)]
+      }
+      hasKeyword <- FALSE
+      for (keyword in suppressBindingKeywords) {
+        if (length(grep(keyword, text)) != 0)
+          hasKeyword <- TRUE
+      }
+      if (!hasKeyword)
+        newNotes <- c(newNotes, notes[i])
+    } else if (regexpr("assigned but may not be used", notes[i]) != -1) {
+      funcDef <- .getFunctionDefinitionFromMem(notes[i])
+      if (is.null(funcDef)) {
+        if (ignoreHiddenFunctions)
+          warning(paste("Ignoring problem in hidden function '",func,"'",sep=""))
+        else
+          newNotes <- c(newNotes, notes[i])
+      } else {
+        variableName <- .getVariableName(notes[i])
+        text <- funcDef[grep(paste("(^|[^$])", variableName, sep = ""), funcDef)]
+        hasKeyword <- FALSE
+        for (keyword in suppressBindingKeywords) {
+          if (length(grep(keyword, text)) != 0)
+            hasKeyword <- TRUE
+        }
+        if (!hasKeyword)
+          newNotes <- c(newNotes, notes[i])
+      }
+    } else if (regexpr("parameter .* may not be used", notes[i]) != -1) {
+      funcDef <- .getFunctionDefinitionFromMem(notes[i])
+      if (is.null(funcDef)) {
+        if (ignoreHiddenFunctions)
+          warning(paste("Ignoring problem in hidden function '",func,"'",sep=""))
+        else
+          newNotes <- c(newNotes, notes[i])
+      } else {
+        if (length(grep("UseMethod\\(",funcDef)) == 0)
+          newNotes <- c(newNotes, notes[i])
+      }
+    } else {
+      newNotes <- c(newNotes, notes[i])
+    }
+  }
+  if (length(newNotes) == 0)
+    writeLines("No problems found") else writeLines(newNotes)
+}
+
---
+++ README.md
@@ -0,0 +1,57 @@
+OhdsiRTools
+===========
+
+Introduction
+============
+An R package with tools for maintaining the other OHDSI R packages
+
+Features
+========
+- Auto code formatting
+- Auto checking of R code
+
+Examples
+===========
+todo
+
+Technology
+============
+OhdsiRTools is an R package.
+
+System Requirements
+============
+Requires R (version 3.1.0 or higher)
+
+Dependencies
+============
+None
+
+Getting Started
+===============
+1. In R, use the following commands to download and install OhdsiRTools:
+
+  ```r
+  install.packages("devtools")
+  library(devtools)
+  install_github("ohdsi/OhdsiRTools")
+  ```
+
+Getting Involved
+=============
+* Package manual: [OhdsiRTools.pdf](https://raw.githubusercontent.com/OHDSI/OhdsiRTools/master/extras/OhdsiRTools.pdf)
+* Developer questions/comments/feedback: <a href="http://forums.ohdsi.org/c/developers">OHDSI Forum</a>
+* We use the <a href="../../issues">GitHub issue tracker</a> for all bugs/issues/enhancements
+
+License
+=======
+OhdsiRTools is licensed under Apache License 2.0
+
+Development
+===========
+OhdsiRTools is being developed in R Studio.
+
+###Development status
+Under development. Do not use.
+
+# Acknowledgements
+- This project is supported in part through the National Science Foundation grant IIS 1251151.
